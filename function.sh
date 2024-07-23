3a1309b68f192811029539706d119f80a8e59c90
vicky <vigneshkumar1002@gmail.com>
2024-07-23 20:31:07 +0530

    function

---
+++ function.sh
@@ -0,0 +1,24 @@
+# function is a name given to a set of commands.
+
+# syntex
+
+#function abc() {
+# command
+# command
+# }
+
+# or
+
+# abc() {
+# command
+# command
+}
+
+# How to call a function - simply call the function as a command
+
+# abc
+
+SAMPLE() {
+ echo SAMPLE FUNCTION
+}
+SAMPLE [now the command whatever inside the () will applicable in code]
