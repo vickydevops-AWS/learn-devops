82cc2b3aeb5bfeca5ea3d597c1c742897996fa09
vicky <vigneshkumar1002@gmail.com>
2024-07-23 20:50:09 +0530

    function

--- function.sh
+++ function.sh
@@ -1,21 +1,4 @@
-# function is a name given to a set of commands.
 
-
-# function abc() {
-# command
-# command
-# }
-
-# or
-
-# abc() {
-# command
-# command
-#}
-
-# How to call a function - simply call the function as a command
-
-# abc
 
 SAMPLE() {
  echo SAMPLE FUNCTION;
