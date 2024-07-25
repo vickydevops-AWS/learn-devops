2144025623656c1f924d0cd5a3d59eb458bcc6a3
vicky <vigneshkumar1002@gmail.com>
2024-07-25 19:39:45 +0530

    Print test

---
+++ 03-print.sh
@@ -0,0 +1,21 @@
+# print color, Here are the color
+# RED         31
+# GREEN       32
+# YELLOW      33
+# BLUE        34
+# MAGENTA     35
+# CYAN        36
+
+
+# syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# -e to enable \e
+# \e to enable the color
+# [COLm - which color to enable, replace COL with 31-36
+# [0m - 0 to reset the color.
+
+echo -e "\e[31mHello word in red color\e[0m"
+echo -e "\e[32mHello word in green color\e[0m"
+echo -e "\e[33mHello word in yellow color\e[0m"
+echo -e "\e[34mHello word in blue color\e[0m"
+echo -e "\e[35mHello word in magenta color\e[0m"
+echo -e "\e[36mHello word in cyan color\e[0m"
